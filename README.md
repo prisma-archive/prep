bbe07785a2ccbc02f237e8cdaf695631e220a06c
Rene Brandel <renebrandel@outlook.com>
2016-08-28 22:05:28 +0200

    Works with three different types of inputs

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # prep [![npm version](https://badge.fury.io/js/prep.svg)](https://badge.fury.io/js/prep)
-Server-side rendering tool for your web app. Prerenders your app into static HTML files and supports routing.
+
+Pre-renders your web app (React, Angular, ...) into static HTML based on your specified routes enabling SEO for single page applications.
 
 ## Install
 
@@ -9,29 +10,69 @@ npm install -g prep
 
 ## Usage
 
-Create a script in your `package.json` and include the following command.
-
 ```sh
-  Usage: prep [options] <build-dir>
+  Usage: prep [options] <build-dir> [<target-dir>]
 
   Options:
 
     -h, --help           output usage information
-    -c, --config [path]  Config file (Default: .preprc)
+    -c, --config [path]  Config file (Default: prep.js)
     -p, --port [port]    Phantom server port (Default: 45678)
 ```
 
-## Example `.preprc`
+A prep config will be used to configure rendering specific details the prep config needs to have the following schema.
 
-```json
-{
-  "routes": [
+```js
+const defaultConfig = {
+  routes: ['/'],
+  timeout: 1000,
+  pagewidth: 1440,
+}
+```
+* `routes` specifies the list of routes that the renderer should pass. (Default: `['/']`)
+* `timeout` is the timeout for how long the renderer should wait for network requests. (Default: `1000`)
+* `pagewidth` the page width in pixels that the renderer should use to render the site. (Default: `1440`)
+
+## Example `prep.js`
+There are different possibilities to return a prep config.
+
+1. Javascript object
+2. Synchronous function
+3. Promise
+
+First and easiest way is to return a Javascript object by itself.
+
+```js
+export default {
+  routes: [
     "/",
     "/world"
   ]
 }
 ```
 
+You can also return a function that returns the prep config.
+
+```js
+export default () => {
+  return {
+    routes: [
+      "/",
+      "/world"
+    ]
+  }
+}
+```
+
+Furthermore you can also return a `Promise` or use `async` & `await`.
+
+```js
+export default async () => {
+  const routes = await getRoutesAsync()
+  return { routes }
+}
+```
+
 ## Known Issues
 
- - If you want to use `Object.assign()` in you code, please add a polyfill like [phantomjs-polyfill-object-assign](https://github.com/chuckplantain/phantomjs-polyfill-object-assign), because prep uses PhantomJS, which doesn't support `Object.assign()` yet.
+ - If you want to use `Object.assign()` in your code, please add a polyfill like [phantomjs-polyfill-object-assign](https://github.com/chuckplantain/phantomjs-polyfill-object-assign), because prep uses PhantomJS, which doesn't support `Object.assign()` yet.
--- src/index.js
+++ src/index.js
@@ -11,6 +11,62 @@ import mkdirp from 'mkdirp'
 import phantom from 'phantom'
 import { exec } from 'child-process-promise'
 
+const util = require('util')
+
+const crawlAndWrite = (configuration) => {
+  const app = express()
+    .use(serveStatic(buildDir))
+    .use(fallback('index.html', { root: buildDir }))
+
+  const server = http.createServer(app).listen(program.port)
+
+  const promises = configuration.routes.map((route) => {
+    let page, instance
+
+    // remove leading slash from route
+    route = route.replace(/^\//, '')
+
+    return phantom.create()
+      .then((_instance) => {
+        instance = _instance
+          return instance.createPage()
+      })
+      .then((_page) => {
+        page = _page
+          return page.open(`http://localhost:45678/${route}`)
+      })
+      .then(() => (
+        page.evaluate(function() {
+          return document.documentElement.outerHTML
+        })
+      ))
+      .then((content) => {
+        const filePath = path.join(tmpDir, route)
+          mkdirp.sync(filePath)
+          fs.writeFileSync(path.join(filePath, 'index.html'), content)
+
+          const logFileName = `${route}/index.html`.replace(/^\//, '')
+            console.log(`prep: Rendered ${logFileName}`)
+
+            page.close()
+            instance.exit()
+      })
+      .catch((error) => {
+        console.log(error)
+        instance.exit()
+      })
+  })
+
+  Promise.all(promises)
+    .catch(() => server.close())
+    .then(() => server.close())
+    .then(() => exec(`cp -rf ${tmpDir}/* ${buildDir}/`))
+    .then(() => exec(`rm -rf ${tmpDir}`))
+    .then(() => process.exit(0))
+}
+
+let babel = require('babel-core')
+
 let config, buildDir, tmpDir
 
 program
@@ -31,57 +87,21 @@ program
 program.parse(process.argv)
 
 try {
-  config = JSON.parse(fs.readFileSync(path.resolve(program.config)))
+  const code = babel.transformFileSync(path.resolve(program.config), {presets: ["es2015"]}).code
+  mkdirp.sync(tmpDir)
+  fs.writeFileSync(path.join(tmpDir, 'prep.js'), code)
+  config = require(path.join(tmpDir, 'prep.js')).default
+
+  if (Promise.resolve(config) === config) {
+    config.then((x) => {
+      crawlAndWrite(x)
+    })
+  } else if (typeof config === 'function') {
+    crawlAndWrite(config())
+  } else {
+    crawlAndWrite(config)
+  }
 } catch (e) {
-  throw new Error(`Couldn't read .preprc config file\n${e}`)
+  throw new Error(e)
 }
 
-const app = express()
-  .use(serveStatic(buildDir))
-  .use(fallback('index.html', { root: buildDir }))
-
-const server = http.createServer(app).listen(program.port)
-
-const promises = config.routes.map((route) => {
-  let page, instance
-
-  // remove leading slash from route
-  route = route.replace(/^\//, '')
-
-  return phantom.create()
-    .then((_instance) => {
-      instance = _instance
-      return instance.createPage()
-    })
-    .then((_page) => {
-      page = _page
-      return page.open(`http://localhost:45678/${route}`)
-    })
-    .then(() => (
-      page.evaluate(function() {
-        return document.documentElement.outerHTML
-      })
-    ))
-    .then((content) => {
-      const filePath = path.join(tmpDir, route)
-      mkdirp.sync(filePath)
-      fs.writeFileSync(path.join(filePath, 'index.html'), content)
-
-      const logFileName = `${route}/index.html`.replace(/^\//, '')
-      console.log(`prep: Rendered ${logFileName}`)
-
-      page.close()
-      instance.exit()
-    })
-    .catch((error) => {
-      console.log(error)
-      instance.exit()
-    })
-})
-
-Promise.all(promises)
-  .catch(() => server.close())
-  .then(() => server.close())
-  .then(() => exec(`cp -rf ${tmpDir}/* ${buildDir}/`))
-  .then(() => exec(`rm -rf ${tmpDir}`))
-  .then(() => process.exit(0))
