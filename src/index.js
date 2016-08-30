e179560286234ff100bfa3d9c537fe83f2da9137
Johannes Schickling <schickling.j@gmail.com>
2016-08-30 14:39:47 +0200

    Removed babel + browserify

--- src/index.js
+++ src/index.js
@@ -11,15 +11,20 @@ import mkdirp from 'mkdirp'
 import phantom from 'phantom'
 import { exec } from 'child-process-promise'
 
-const util = require('util')
+let buildDir, targetDir, tmpDir
 
 const crawlAndWrite = async (configuration) => {
 
   let dimensions = Object.assign({}, {width: 1440, height: 900}, configuration.dimensions)
   delete configuration.dimensions
-  configuration = Object.assign({}, {
-    routes: ['/'], timeout: 1000, dimensions
-  }, configuration)
+  configuration = Object.assign(
+    {
+      routes: ['/'],
+      timeout: 1000,
+      dimensions,
+    },
+    configuration
+  )
 
   const app = express()
     .use(serveStatic(buildDir))
@@ -30,16 +35,19 @@ const crawlAndWrite = async (configuration) => {
   const promises = configuration.routes.map(async (route) => {
     // remove leading slash from route
     route = route.replace(/^\//, '')
-    const instance = await phantom.create()
+
+    const phantomOptions = ['--disk-cache=true']
+
+    const instance = await phantom.create(phantomOptions)
     const page = await instance.createPage()
     page.property('viewportSize', {width: configuration.dimensions.width, height: configuration.dimensions.height})
     await page.open(`http://localhost:${program.port}/${route}`)
     const content = await new Promise((resolve) => {
-          setTimeout(() => resolve(page.evaluate(
-              () => document.documentElement.outerHTML,
-              configuration.timeout)
-          ))
-        })
+      setTimeout(() => resolve(page.evaluate(
+          () => document.documentElement.outerHTML,
+          configuration.timeout)
+      ))
+    })
     const filePath = path.join(tmpDir, route)
     mkdirp.sync(filePath)
     fs.writeFileSync(path.join(filePath, 'index.html'), content)
@@ -55,20 +63,16 @@ const crawlAndWrite = async (configuration) => {
 
   await Promise.all(promises)
   server.close()
-  await exec(`rm -f ${tmpDir}/prep.js`)
   await exec(`cp -rf ${tmpDir}/* ${targetDir}/`)
   await exec(`rm -rf ${tmpDir}`)
   process.exit(0)
 }
 
-let babel = require('babel-core')
-
-let config, buildDir, targetDir, tmpDir
-
 program
+  .version('1.0.1')
   .description('Server-side rendering tool for your web app.\n  Prerenders your app into static HTML files and supports routing.')
   .arguments('<build-dir> [target-dir]')
-  .option('-c, --config [path]', 'Config file (Default: .preprc)', '.preprc')
+  .option('-c, --config [path]', 'Config file (Default: prep.js)', 'prep.js')
   .option('-p, --port [port]', 'Phantom server port (Default: 45678)', 45678)
   .action((bdir, tdir) => {
     if (!bdir) {
@@ -83,23 +87,28 @@ program
 
 program.parse(process.argv)
 
-const prepareConfig = async () => {
-  try {
-    const code = babel.transformFileSync(path.resolve(program.config), {presets: ["es2015", "stage-0"], plugins: ["transform-runtime"]}).code
-    mkdirp.sync(tmpDir)
-    fs.writeFileSync(path.join(tmpDir, 'prep.js'), code)
-    config = require(path.join(tmpDir, 'prep.js')).default
+//const run = () => {
+  //const configFilePath = path.resolve(program.config)
+  //const babelOptions = {
+    //presets: ['es2015', 'stage-0'],
+    //plugins: ['transform-runtime'],
+    //babelrc: false,
+  //}
+  // transpile config file to ES5
+  //const { code } = babel.transformFileSync(configFilePath, babelOptions)
 
-    if (Promise.resolve(config) === config) {
-      crawlAndWrite(await config)
-    } else if (typeof config === 'function') {
-      crawlAndWrite(config())
-    } else {
-      crawlAndWrite(config)
-    }
-  } catch (e) {
-    throw new Error(e)
+  // write transpiled code to temp file and require it
+  //const writeStream = fs.writeFileSync('.prep.js', code)
+  const config = require(path.resolve(program.config)).default
+
+  if (Promise.resolve(config) === config) {
+    config.then((c) => crawlAndWrite(c))
+    //crawlAndWrite(await config)
+  } else if (typeof config === 'function') {
+    crawlAndWrite(config())
+  } else {
+    crawlAndWrite(config)
   }
-}
+//}
 
-prepareConfig()
+//run()
