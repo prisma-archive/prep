6c5944826566f49a44951be0817ff25fc51aa030
Johannes Schickling <schickling.j@gmail.com>
2017-08-16 12:12:42 +0200

    Refactor host

--- src/index.js
+++ src/index.js
@@ -107,8 +107,10 @@ async function prepRoute(route, configuration) {
     })
 
     debug('Nightmare started')
-
-    const url = (configuration.hostname ? (configuration.hostname + "/") : (`http${configuration.https ? 's' : ''}://localhost:${program.port}/`)) + `${route}`
+    
+    const host = configuration.hostname ? configuration.hostname : `http${configuration.https ? 's' : ''}://localhost:${program.port}`
+    const url = `${host}/${route}`
+    
     const content = await nightmare
         .useragent(configuration.useragent)
         .viewport(configuration.dimensions.width, configuration.dimensions.height)
